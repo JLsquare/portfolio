102f9852e228a6672bf83b937df5b6c03ff2fe34
JL² <jeanloup.mellion@gmail.com>
2023-03-05 14:47:12 +0100

    port 80

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'src')));
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+app.listen(80, () => {
+    console.log('Server running on port 80');
 });
